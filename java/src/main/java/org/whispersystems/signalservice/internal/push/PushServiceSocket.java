7d334daa9f40843679af2a62ab2eb6648209e2ab
Konglomerat <konglomerat@protonmail.com>
2019-08-08 21:17:12 +0700

    Patch to make sure upload process of attachment to Amazon S3 bucket through CloudFront is OK

--- java/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
+++ java/src/main/java/org/whispersystems/signalservice/internal/push/PushServiceSocket.java
@@ -136,7 +136,7 @@ public class PushServiceSocket {
   private static final String SENDER_CERTIFICATE_PATH   = "/v1/certificate/delivery";
 
   private static final String ATTACHMENT_DOWNLOAD_PATH  = "attachments/%d";
-  private static final String ATTACHMENT_UPLOAD_PATH    = "attachments/";
+  private static final String ATTACHMENT_UPLOAD_PATH    = "";
 
   private static final String STICKER_MANIFEST_PATH     = "stickers/%s/manifest.proto";
   private static final String STICKER_PATH              = "stickers/%s/full/%d";
